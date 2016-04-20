cd2ecb328f9fef9bf41aa3ee5ab93dc0811f61e8
Florian Pichler <florianlukas.pichler@hs-augsburg.de>
2016-04-20 15:05:59 +0200

    Interface EntityContext
    Methoden impl.

--- src/prog2_a3/FlattenedBoard.java
+++ src/prog2_a3/FlattenedBoard.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import prog2_a3.fatsquirrel.core.*;
 
 
-public class FlattenedBoard implements prog2_a3.interfaces.BoardView {
+public class FlattenedBoard implements prog2_a3.interfaces.BoardView, prog2_a3.interfaces.EntityContext {
     
  
     private final Entity[][] flattenedBoard;
@@ -26,4 +26,64 @@ public class FlattenedBoard implements prog2_a3.interfaces.BoardView {
     public Entity getField(int x, int y){
         return this.flattenedBoard[x][y];
     }
+
+	@Override
+	public XY getSize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public XY getEntityType(XY xy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PlayerEntity nearestPlayerEntity(XY xy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void tryMove(MiniSquirrel miniSquirrel, XY moveDirection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tryMove(GoodBeast goodBeast, XY moveDirection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tryMove(BadBeast badBeast, XY moveDirection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tryMove(MasterSquirrel masterBot, XY moveDirection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void spawnChildBot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void kill(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void killAndReplace(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/prog2_a3/interfaces/EntityContext.java
+++ src/prog2_a3/interfaces/EntityContext.java
@@ -1,10 +1,35 @@
 package prog2_a3.interfaces;
 
+import prog2_a3.fatsquirrel.core.BadBeast;
+import prog2_a3.fatsquirrel.core.Entity;
+import prog2_a3.fatsquirrel.core.GoodBeast;
+import prog2_a3.fatsquirrel.core.MasterSquirrel;
+import prog2_a3.fatsquirrel.core.MiniSquirrel;
+import prog2_a3.fatsquirrel.core.PlayerEntity;
 import prog2_a3.fatsquirrel.core.XY;
 
 public interface EntityContext {
 
-	XY getSize();
 	
+	public XY getSize();
 	
+	public XY getEntityType(XY xy);
+	
+	public PlayerEntity nearestPlayerEntity(XY xy);
+	
+	void tryMove(MiniSquirrel miniSquirrel, XY moveDirection);
+	
+	void tryMove(GoodBeast goodBeast, XY moveDirection);
+	
+	void tryMove(BadBeast badBeast, XY moveDirection);
+	
+	void tryMove(MasterSquirrel masterBot, XY moveDirection);
+	
+	void spawnChildBot(); //Parameter nicht sicher
+	
+	void kill(Entity entity);
+	
+	void killAndReplace(Entity entity);
+	
+
 }
